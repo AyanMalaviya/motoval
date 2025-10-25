67883d9c9984dbe716dd8175cbf723af0493f1d2
AyanMalaviya <ayanmalaviya111@gmail.com>
2025-10-25 16:33:10 +0530

    Fixed event unused error

--- src/components/auth/RegisterForm.tsx
+++ src/components/auth/RegisterForm.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
-import Input from '../ui/Input';
-import Button from '../ui/Button';
+import Input from '../common/UI/Input';
+import Button from '../common/UI/Button';
 
 const RegisterForm: React.FC = () => {
   const [formData, setFormData] = useState({
--- src/components/car/CarFilters.tsx
+++ src/components/car/CarFilters.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from '../ui/Button';
+import Button from '../common/UI/Button';
 import type { CarFilters } from '../../types/car';
 
 interface CarFiltersComponentProps {
